514d6732ce68a3a9bb97e61fb568dc964f5e8805
Amina Radoni <aminaradoni24@gmail.com>
2024-05-22 19:38:03 +0200

    Two more Serverless function

---
+++ functions/products.js
@@ -0,0 +1,8 @@
+// /functions/products.js
+
+exports.handler = async (event, context, cb) => {
+  return {
+    statusCode: 200,
+    body: "products",
+  }
+}
---
+++ functions/single-product.js
@@ -0,0 +1,7 @@
+// /functions/single-product.js
+exports.handler = async function (event, context, cb) {
+  return {
+    statusCode: 200,
+    body: "single product",
+  }
+}

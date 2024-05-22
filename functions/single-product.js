14fcfd0072286d575f7f1cd962e3a9728386386a
Amina Radoni <aminaradoni24@gmail.com>
2024-05-22 23:37:19 +0200

    Single Product - Structure

--- functions/single-product.js
+++ functions/single-product.js
@@ -1,7 +1,15 @@
 // /functions/single-product.js
 exports.handler = async function (event, context, cb) {
+  console.log(event.queryStringParameters)
+  const { id } = event.queryStringParameters
+  if (id) {
+    return {
+      statusCode: 200,
+      body: "single product",
+    }
+  }
   return {
-    statusCode: 200,
-    body: "single product",
+    statusCode: 400,
+    body: "please provide product id",
   }
 }

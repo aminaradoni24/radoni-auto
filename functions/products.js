0c1a25f2958816d6561bfa90cc8c739d35a22439
Amina Radoni <aminaradoni24@gmail.com>
2024-05-22 23:25:21 +0200

    Refactor and return product

--- functions/products.js
+++ functions/products.js
@@ -12,13 +12,37 @@ exports.handler = async function () {
   try {
     const response = await airtable.list({ maxRecords: 200 })
 
-    console.log("#######")
-    console.log(response)
-    console.log("#######")
+    const products = response.records.map((product) => {
+      const { id, fields } = product
+      const {
+        name,
+        featured,
+        price,
+        colors,
+        company,
+        description,
+        category,
+        shipping,
+        images,
+      } = fields
+      const { url } = images[0]
+      return {
+        id,
+        name,
+        featured,
+        price,
+        colors,
+        company,
+        description,
+        category,
+        shipping,
+        image: url,
+      }
+    })
 
     return {
       statusCode: 200,
-      body: "products route",
+      body: JSON.stringify(products),
     }
   } catch (error) {
     console.log(error)
